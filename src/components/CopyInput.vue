c2254278dc2d1becc411c04a3db0d1a3216daf37
Cosmos <cosmos-s@hotmail.com.tw>
2023-08-18 16:00:27 +0800

    ✨ Feat: update

--- src/components/CopyInput.vue
+++ src/components/CopyInput.vue
@@ -1,7 +1,27 @@
 <script setup>
 import { computed, ref, watch } from "vue";
 
-const inputValue = ref("");
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: "",
+  },
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+const inputValue = ref(props.modelValue);
+
+const updateValue = (value) => {
+  emit("update:modelValue", value);
+};
+
+watch(
+  () => props.inputValue,
+  (value) => {
+    updateValue(value)
+  }
+);
 
 const handleClick = () => {
     if (!inputValue.value) {
