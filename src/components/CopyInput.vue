d55035e47ce65d94e603d683ebc30fae3f14fc5b
Cosmos <cosmos-s@hotmail.com.tw>
2023-08-18 15:12:55 +0800

    ✨ Feat: #1 create copy input file

---
+++ src/components/CopyInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <input></input>
+</template>
+
+<script>
+export default {
+  name: 'CopyInput',
+  data() {
+    return {
+
+    };
+  },
+
+  mounted() {
+
+  },
+
+  methods: {
+
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
