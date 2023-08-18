f474f752bf11b13d305314b51e2a88d87bcf5ffd
Nick Lai <resxc13579@gmail.com>
2023-08-18 16:09:35 +0800

    feat: 新增 input 字數提示

---
+++ src/components/CharCountInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input type="text" @input="count = $event.target.value.length ?? 0">
+    <span>
+      <small>{{ count }} / {{ options.maxLength }}</small>
+    </span>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+  options: {
+    type: Object,
+    default: () => ({}),
+  },
+});
+
+const count = ref(0);
+</script>
+
+<style scoped>
+  span {
+    margin-left: 10px;
+  }
+</style>
